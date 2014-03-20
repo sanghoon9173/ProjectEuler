f6b94bcea93e943d0046b54f02f5d1a6583ad7df
sanghoon <sh42lee@uwaterloo.ca>
2014-03-20 02:07:38 -0400

    problem 4 solved

--- question4.py
+++ question4.py
@@ -7,26 +7,23 @@ def LargestPalindromeProduct():
 	subtractor1 = 1100	#every 1
 	subtractor2 = 110	#every 10th
 	subtractor3 = 11 	#every 100th
-	divider = 999
+	palindromicNumb = 999999
+	counter = 1
 
-	while(divider>100):
-		counter=1
-		palindromicNumb = 999999
-		while(1==1):
+	while(palindromicNumb>100000):
+		divider = 999 #reset divider
+		while(divider>100):
 			if(palindromicNumb%divider==0 and palindromicNumb/divider < 1000):
 				return "Palindromic Number: " + str(palindromicNumb) + " with values of " + str(divider) +" and "+ str(palindromicNumb/divider) 
-			if(counter%10==0):
-				if(counter%100==0):
-					palindromicNumb -= subtractor3
-				else:
-					palindromicNumb -= subtractor2
+			divider-=1
+		if(counter%10==0):
+			if(counter%100==0):
+				palindromicNumb -= subtractor3
 			else:
-				palindromicNumb -= subtractor1
-			if(palindromicNumb<100000):
-				break
-			print palindromicNumb
-			counter+=1
-		divider-=1
+				palindromicNumb -= subtractor2
+		else:
+			palindromicNumb -= subtractor1
+		counter+=1
 
 	return "ERROR: SUCH PALINDROMIC NUMBER NOT FOUND"
 
